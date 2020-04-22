9de15677de294e7758084bc546ed6098427b4ca3
Eduardo Ali Garcia Ponce <ali_524-@hotmail.com>
2020-04-22 09:54:55 -0500

    Terminado, el archivo manda "Jorge de respuesta"

---
+++ connection.php
@@ -0,0 +1,29 @@
+<?php
+require 'vendor/autoload.php';
+
+$hosts = ['https://elastic:ZH8bWoMLTUobqR4lLM60hVl4@1624ac56bdb14b76a4a42fb9b47cfe77.us-east-1.aws.found.io:9243'];
+
+$client = Elasticsearch\ClientBuilder::create()
+					->setHosts($hosts)
+					->build();
+
+$params = [
+		'index' => 'idxdvcovid001',
+];
+
+$response = $client->search($params);
+
+$hits = count($response['hits']['hits']);
+$result = null;
+$i = 0;
+
+while ($i < $hits){
+			$result[$i] = $response['hits']['hits'][$i]['_source'];
+			$i++;
+}
+
+foreach ($result as $key => $value){
+			echo $value['Name'] . "<br>";
+}
+
+?>
