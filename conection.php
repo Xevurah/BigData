2f1e07400bba3691c0c9501c80d7c6940300dd22
Eduardo Ali Garcia Ponce <ali_524-@hotmail.com>
2020-05-25 20:06:27 -0500

    Cambios, Merge con Errores anteriores

--- conection.php
+++ conection.php
@@ -5,6 +5,10 @@ $hosts =  ['https://elastic:ZH8bWoMLTUobqR4lLM60hVl4@1624ac56bdb14b76a4a42fb9b47
 // Kibana
 //https://2bbe200001a241a5bdbbf0e45db1b3da.us-east-1.aws.found.io:9243/app/kibana#/dev_tools/console
 //elastic
+//YA 
+$client = Elasticsearch\ClientBuilder::create()
+                    ->setHosts($hosts)
+                    ->build();
 $params = [
     'index' => 'idxdvcovid001'
 ];
